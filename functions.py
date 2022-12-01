ec3fc867666e01111ee735dd2c80ba5c98c6c8d1
Pqmeu <rdesouzamatheus@gmail.com>
2022-12-01 00:20:59 -0300

    Comitando todos os arquivos do meu projeto de cadastro e boletim de alunos

---
+++ functions.py
@@ -0,0 +1,99 @@
+import sqlite3
+from sqlite3 import Error
+from tkinter import messagebox
+import os
+
+#função para conexão num banco de dados sqlite3
+def criarConexao():
+    path = "C:\\Users\\Matheus\\Documents\\projetoBOLETIM\\banco\\boletimDB.db"
+    con = None
+    try:
+        con = sqlite3.connect(path)
+    except Error as ex:
+        print(ex)
+    return con
+
+def select():
+    conexao = criarConexao()
+    sql = "SELECT * from tb_boletim"
+    c=conexao.cursor()
+    c.execute(sql)
+    resultado=c.fetchall()
+    return resultado
+
+def resetar():
+    r = messagebox.askquestion("AVISO!!", "APAGAR TODOS OS DADOS DE ALUNOS?")
+    if r != "yes":
+        return
+    else:
+        try:
+            c=criarConexao()
+            c.execute("DELETE FROM tb_boletim")
+            c.commit()
+            c.close()
+        except Error as ex:
+            print(ex)
+
+def deletar(treeView):
+    r = messagebox.askquestion("AVISO!", "DELETAR ESTE ALUNO DO BANCO DE DADOS?")
+    if (r == "no"):
+        return
+    else:
+        try:
+            line = treeView.selection()[0]
+            vline = str(treeView.item(line)['values'][0])
+            c=criarConexao()
+            cursor = c.cursor()
+            cursor.execute("DELETE FROM tb_boletim WHERE N_MATRICULA = '"+str(vline)+"' ")
+            print(vline)
+            c.commit()
+            c.close()
+            print("aluno excluido")
+        except Error as ex:
+            print(ex)
+
+def limpar(n, n1, n2):
+    n.delete(0, 'end')
+    n1.delete(0, 'end')
+    n2.delete(0, 'end')
+    n.focus_set()
+
+
+def consultarColuna():
+    try:
+        sql = "SELECT COUNT(*) from tb_boletim"
+        con = criarConexao()
+        cursor = con.cursor()
+        cursor.execute(sql)
+        resultado=cursor.fetchall()
+        print ("tens um total de: ", resultado[0][0], "alunos")
+        return resultado[0][0]
+    except Error as ex:
+        print(ex)
+
+def novoCadastro():
+    pastaApp=os.path.dirname(__file__)
+    exec(open(pastaApp+"\\menuCadastro.py").read())
+    
+#vai ler o arquivo de texto com a matricula selecionada no treeview do maincode
+def lerArquivo():
+    arquivo = open("arquivo.txt", "r")
+    vArquivo = arquivo.read()
+    print("esta é a matricula no arquivo de texto: ",vArquivo)
+    return vArquivo
+
+def selectVariado():
+    matri = lerArquivo()
+    con = criarConexao()
+    cursor = con.cursor()
+    cursor.execute("SELECT * FROM tb_boletim WHERE N_MATRICULA = '"+matri+"'")
+    resultado=cursor.fetchall()[0][0]
+    print("matricula no banco de dados: ",resultado, "     classe da variavel da matricula", type(resultado))
+    return resultado
+    
+# ssql = "SELECT * FROM tb_boletim"
+# res = functions.select(vcon, ssql)
+# cont = 0
+# for r in res:
+#     cont += 1
+#     Label(wn, text=r[1]).grid(column=0, row=5+cont)
---
+++ maincode.py
@@ -0,0 +1,123 @@
+from sqlite3 import Error
+from tkinter import *
+from tkinter import ttk
+from tkinter import messagebox
+import functions
+from functions import select
+
+
+#função para abrir nova janela
+# pastaApp=os.path.dirname(__file__)
+# def novoCadastro():
+#     exec(open(pastaApp+"\\menuCadastro.py").read())
+
+#escreve a matricula selecionada num arquivo de texto
+
+
+#atualiza a treeview
+def refreshTable():
+    for data in treeView.get_children():
+        treeView.delete(data)
+    for array in select():
+        treeView.insert(parent='', index='end', iid=array, text="", values=(array), tag='orow')
+    treeView.tag_configure('orow', font=('Arial', 12))
+    treeView.grid(column=0, row=1, padx=10, pady=20)
+
+def cadastrar(conexao):
+    n = nome.get()
+    matri = matricula.get()
+    vcpf = cpf.get()
+    if (n == "" or n == " "):
+        messagebox.showinfo("ERRO", "O NOME DO ALUNO NÃO PODE ESTAR VAZIO")
+        return
+    else:
+        vsql = "INSERT INTO tb_boletim (N_MATRICULA, T_NOMEALUNO, T_CPF) VALUES ('"+matri+"', '"+n+"', '"+vcpf+"')"
+        try:
+            c=conexao.cursor()
+            c.execute(vsql)
+            conexao.commit()
+            print("aluno inserido")
+            refreshTable()
+        except Error as ex:
+            print(ex)
+            
+def consultarTreeview(mtreeView):        
+    linha = mtreeView.selection()[0]
+    mar = mtreeView.item(linha)['values'][0]
+    print(mar)
+    escreverArquivo(mar)
+    return mar
+
+def escreverArquivo(matr):
+    arquivo = open("arquivo.txt", "w")
+    vArquivo = arquivo.write(str(matr))
+    return vArquivo
+
+#criacao da janela em tkinter
+wn = Tk()
+wn.title("Sistema de Boletim")
+wn.geometry("400x300")
+
+treeView = ttk.Treeview(wn)
+
+style = ttk.Style()
+style.configure("Treeview.Heading")
+
+treeView['columns'] = ("Matricula", "Nome")
+
+treeView.column("#0", width=0, stretch=NO)
+treeView.column("Matricula", anchor=W, width=100)
+treeView.column("Nome", anchor=W, width=80)
+
+
+treeView.heading("Matricula", anchor=W, text="Matricula")
+treeView.heading("Nome", anchor=W, text="Nome")
+
+refreshTable()
+
+Label(wn, text="Alunos cadastrados:").grid(column=0, row=0)
+
+
+#cria conexao com o banco de dados
+vcon=functions.criarConexao()
+
+#entry do nome do aluno
+Label(wn, text="Nome do aluno:").place(x=285, y=2)
+nome = Entry(wn)
+nome.place(x=270, y=20)
+
+#entry da primeira nota do aluno
+Label(wn, text="Matricula:").place(x=310, y=40)
+matricula = Entry(wn)
+matricula.place(x=270, y=60)
+
+#entry da segunda nota do aluno
+Label(wn, text="CPF:").place(x=310, y=80)
+cpf = Entry(wn)
+cpf.place(x=270, y=100)
+
+#botão para adicionar novo aluno
+adicionar = ttk.Button(wn, text="Adicionar novo aluno", command=lambda: [cadastrar(vcon), functions.limpar(nome, matricula, cpf)])
+adicionar.place(x=270, y=130)
+
+#botão para abrir nova janela com dados do aluno selecinado na treeview
+abrir = ttk.Button(wn, text="Selecionar aluno", command=lambda: [consultarTreeview(treeView), functions.novoCadastro()])
+abrir.place(x=270, y=160)
+
+#botão para remover aluno
+remover = ttk.Button(wn, text="Remover aluno", command=lambda: [functions.deletar(treeView), refreshTable()])
+remover.place(x=270, y=190)
+
+#botão que apaga todos os dados do banco de dados
+resetar = ttk.Button(wn, text="Apagar tudo", command=lambda:[functions.resetar(), refreshTable()])
+resetar.place(x=270, y=220)
+
+#botão de teste que vai printar no console os dados da linha selecionada no treeview
+printar = ttk.Button(wn, text="contar alunos", command=lambda:[functions.consultarColuna()])
+printar.place(x=8, y=270)
+
+#fecha a janela do boletim
+fechar = ttk.Button(wn, text="fechar boletim", command=wn.destroy)
+fechar.place(x=270, y=280)
+
+wn.mainloop()
